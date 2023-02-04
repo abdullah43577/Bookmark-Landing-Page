25a76ef31171f71c262d7117c9758e3548f6a36a
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-02-04 20:43:15 +0100

    working on UI for both mobile and desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   content: ["./*.html", "./app/*.js"],
   theme: {
-    screen: {
+    screens: {
       sm: "480px",
       md: "768px",
       lg: "976px",
-      xl: "1440",
+      xl: "1240px",
     },
     extend: {
       colors: {
